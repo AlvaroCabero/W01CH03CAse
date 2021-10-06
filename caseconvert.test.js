8372cbc703f7667824f0a313f7ec44f3bb09dfb7
Alvaro Cabero <alvarocabero@gmail.com>
2021-10-06 16:58:43 +0200

    :arrow_up: Added test file and edited .editorconfig

---
+++ caseconvert.test.js
@@ -0,0 +1,18 @@
+function caseConvert(texto) {
+  return Object.is(a, b);
+}
+
+describe("funtion caseConvert", () => {
+  test("When it receives texto, it returns texto fo", () => {
+    //Arrange
+    const firstValue = 1;
+    const secondValue = 1;
+    const expected = true;
+
+    //Act
+    const result = strictEquals(firstValue, secondValue);
+
+    //Asset
+    expect(result).toBe(expected);
+  });
+});
